bf2fda6f21f0ed2682b99361cfa500ac9baf80e3
Karinaguseva <106375372+Karinaguseva@users.noreply.github.com>
2022-10-14 12:13:17 +0300

    feat: implement simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      const length = this.array.length;
+      return length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+      this.array.push(`( ${value} )`);
+      return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+      if (this.array[position - 1] == undefined) {
+          this.array = [];
+          throw new Error(`You can't remove incorrect link!`);
+      } else {
+          this.array.splice(position - 1, 1);
+          return this;
+      }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      this.array.reverse();
+      return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      const separator = this.array.join('~~');
+      this.array = [];
+      return separator;
   }
 };
 
