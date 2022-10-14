0ff820a0858c14fa1cd195acdb600bf61fbeb242
Karinaguseva <106375372+Karinaguseva@users.noreply.github.com>
2022-10-14 14:48:35 +0300

    feat: implement mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  let num = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if(result[i][j] == true){
+      result[i][j] = 1
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if ((matrix[i - 1] && matrix[i - 1][j]) == true) {
+          num++;
+      }
+      if ((matrix[i - 1] && matrix[i - 1][j - 1]) == true) {
+          num++;
+      }
+      if ((matrix[i - 1] && matrix[i - 1][j + 1]) == true) {
+          num++;
+      }
+      if ((matrix[i] && matrix[i][j - 1]) == true) {
+          num++;
+      }
+      if ((matrix[i] && matrix[i][j + 1]) == true) {
+          num++;
+      }
+      if ((matrix[i + 1] && matrix[i + 1][j]) == true) {
+          num++;
+      }
+      if ((matrix[i + 1] && matrix[i + 1][j - 1]) == true) {
+          num++;
+      }
+      if ((matrix[i + 1] && matrix[i + 1][j + 1]) == true) {
+          num++;
+      }
+      result[i][j] = num;
+      num = 0;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
