81fb651d1d361009a06d5d33e30683d5899b80c6
Karinaguseva <106375372+Karinaguseva@users.noreply.github.com>
2022-10-14 14:47:36 +0300

    feat: implement dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let array = [];
+  for(let i = 0; i < domains.length; i++){
+      let domain = [];
+      let firstArr = domains[i].split('.').reverse();
+      let str = '';
+      for(let j = 0 ; j < firstArr.length; j++){
+          str = str + '.' + firstArr[j];
+          domain.push(str);
+      }
+      array.push(domain);
+  }
+  array = array.reverse().flat();
+  let result = {};
+  for (let i = 0; i < array.length; ++i){
+      let arrayI = array[i];
+      if (result[arrayI] != undefined)
+          ++result[arrayI];
+      else
+          result[arrayI] = 1;
+  }
+  return result
 }
 
 module.exports = {
